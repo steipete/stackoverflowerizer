527fcaec42a517a484d709a73a8a4b9a09bc6e65
Paul Conroy <paul@daft.ie>
2012-02-12 18:45:52 +0000

    Prevent js errors on non-q pages on djira

--- Chrome/js/redirect_drija.js
+++ Chrome/js/redirect_drija.js
@@ -3,6 +3,13 @@
  * questions as well as SO. Regex on the source question is thus a
  * little sloppier and more prone to false positives, so it's spun
  * out separate to the regex for the other sites to focus on drija alone.
+ *
+ * Two options here: Url regex to determine if we're on q page (of form
+ * djira.com/TOPIC-WORDS/ID/TITLE) or attempt the queryselector and just
+ * see if we've a result. Going with the latter here..
  */
 var soResults = document.querySelectorAll('a[href*=".com/questions/"]');
-document.location = soResults.item(0).href;
+if(soResults.item(0))
+{
+    document.location = soResults.item(0).href;
+}
